9c42590aec905827f42137f20b801243ac07931e
mamerida <mamerida2018@gmail.com>
2024-02-22 21:00:58 -0300

    [PATCH] Add main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import BB8 from '../../public/bb8.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center bg-gray-900">
+      <section className="flex flex-col items-center gap-20 ">
+        <Image src={BB8}  className="m-0 p-0" alt="placeholder" width={"auto"} height={500} priority={false} />
+        <span className="text-slate-200 font-[800] text-7xl text-center">Find your next greate adventure</span>
+      </section>
     </main>
   );
 }
